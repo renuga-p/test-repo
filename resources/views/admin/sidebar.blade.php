c390f049592cc8e030ac4ae5253b07d8e3417bed
renuga-p <preethikalpana4512@gmail.com>
2019-07-15 12:36:16 +0530

    first commit

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,24 @@
+<div class="col-md-3">
+    <div class="card">
+        <div class="card-header">
+            Sidebar
+        </div>
+
+        <div class="card-body">
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/home') }}">
+                        Dashboard
+                    </a>
+                </li>
+            </ul>
+            <ul class="nav" role="tablist">
+                <li role="presentation">
+                    <a href="{{ url('/admin/posts') }}">
+                        Posts
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
